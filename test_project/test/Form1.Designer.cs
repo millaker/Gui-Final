45aaf1e5e78b938656c1e1dc5e1421cba9ab208e
disneyyy <jordan031039@gmail.com>
2021-12-07 18:22:59 +0800

    123
    1111111

--- test_project/test/Form1.Designer.cs
+++ test_project/test/Form1.Designer.cs
@@ -31,11 +31,13 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1044, 624);
+            this.ClientSize = new System.Drawing.Size(642, 390);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
